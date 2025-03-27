9b0b042de891b1a893cc0899e407971bcc8a41a4
DarkSharpness <2040703891@qq.com>
2025-03-27 19:36:15 +0800

    feat(hw7): complete all

--- python/hw7.py
+++ python/hw7.py
@@ -50,7 +50,71 @@ def p2_b():
     fsm.add_edges(2, (1, a), (2, a_and_b), final=True)
     fsm.dump_to(name="p2_b", dir=__file__)
 
+def p3_d():
+    a_and_not_b = "a∧¬b"
+    a_and_b = "a∧b"
+    not_b = "¬b"
+    not_a_and_b = "¬a∧b"
+    true = "true"
+    labels = [a_and_not_b, a_and_b, not_b, not_a_and_b, true]
+    fsm = FSM(label=labels, type_="NFA")
+    fsm.add_edges(0, (0, true), (1, a_and_not_b), start=True)
+    fsm.add_edges(1, (1, not_b), (2, a_and_b), (3, not_a_and_b), final=True)
+    fsm.add_edges(2, (2, true))
+    fsm.add_edges(3, (3, true), final=True)
+    fsm.dump_to(name="p3_d", dir=__file__)
+
+def p3_e():
+    non = ""
+    labels = [non]
+    fsm = FSM(label=labels, type_="NFA")
+    fsm.add_edges(0, (1, non), (3, non), (4, non), start=True)
+    fsm.add_edges(1, (2, non))
+    fsm.add_edges(3, (0, non))
+    fsm.add_edges(2, (3, non), (2, non), (4, non))
+    fsm.add_edges(4, (5, non), final=True)
+    fsm.add_edges(5, (4, non), (7, non), final=True)
+    fsm.add_edges(6, (7, non), (9, non), final=True)
+    fsm.add_edges(7, (8, non), final=True)
+    fsm.add_edges(9, (6, non), final=True)
+    fsm.add_edges(8, (9, non), (8, non), final=True)
+    _name_map = {
+        0: "(s{0}0{1}, q{0}0{1})",
+        1: "(s{0}1{1}, q{0}0{1})",
+        2: "(s{0}2{1}, q{0}0{1})",
+        3: "(s{0}3{1}, q{0}0{1})",
+        4: "(s{0}3{1}, q{0}1{1})",
+        5: "(s{0}0{1}, q{0}1{1})",
+        6: "(s{0}0{1}, q{0}3{1})",
+        7: "(s{0}1{1}, q{0}3{1})",
+        8: "(s{0}2{1}, q{0}3{1})",
+        9: "(s{0}3{1}, q{0}3{1})"
+    }
+
+    _new_map = {}
+    for k, v  in _name_map.items():
+        # surround number with <sub> </sub> tag
+        _new_map[k] = v.format("<sub>", "</sub>")
+    fsm._name_map = _new_map
+
+    fsm.dump_to(
+        name="p3_e",
+        dir=__file__,
+        rank_group=[
+            [0, 1], [2, 3], [4, 5], [6, 7], [8, 9]
+        ],
+        edge_attr_map={
+            (2, 2): {"headport": "nw", "tailport": "sw"},
+            (8, 8): {"headport": "nw", "tailport": "sw"},
+        },
+        fsm_attr_map={
+            "rankdir": "NW"
+        },
+    )
+
 p1_a()
 p1_b()
 p1_c()
 p2_b()
+p3_d()
+p3_e()
